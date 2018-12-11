093a123236a5a92da35904240745114544bee331
Peter Krivan <krivan.peter@outlook.com>
2018-12-11 16:32:30 +0000

    Check the answers. If right next question, if not same question with the wrong answer below the riddle

--- game.py
+++ game.py
@@ -2,15 +2,26 @@ import difflib
 import random
 
 questions = []
+answers = []
 
-def get_question():
+def open_riddles():
     with open("data/riddles.txt", "r") as file:
         lines = file.read().splitlines()
-        
     for i, text in enumerate(lines):
         if i%2 == 0:
             questions.append(text)
-    
-    #question = questions[random.randint(0,(len(questions)-1))]
-    question = random.choice(questions)
+        else:
+            answers.append(text)
+
+def get_question():
+    question = questions[len(questions)-1]
     return question
+
+def check_answer(answer):
+    points = 0
+    answers.append(answer)
+    if answers[len(questions)-1] == answer.lower():
+        points += 1
+        return True
+    else:
+        return False
--- run.py
+++ run.py
@@ -3,22 +3,18 @@ from flask import Flask, redirect, render_template, request
 from game import *
 
 app = Flask(__name__)
-answers = []
 
 def write_to_file(filename, data):
     """Handle the process of writing data to a file"""
     with open(filename, "a") as file:
         file.writelines(data)
-        
-def add_answer(username, answer):
-    """Add answers to the `answers` text file"""
-    write_to_file("data/answers.txt", "{0}".format(answer))
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
     if request.method == "POST":
         write_to_file("data/users.txt", request.form["username"] + "\n")
+        open_riddles()
         return redirect(request.form["username"])
     return render_template("index.html")
 
@@ -26,9 +22,16 @@ def index():
 @app.route('/<username>', methods=["GET", "POST"])
 def user(username):
     if request.method == "POST":
-        add_answer(username, request.form["answer"] + "\n")
-    question = get_question()
-    return render_template("game.html", question=question)
+        answer = request.form["answer"]
+        if check_answer(answer):
+            questions.pop()
+            question = get_question()
+        else:
+            question = get_question()
+            return render_template("game.html", question = question, answer = answer)
+    else:
+        question = get_question()
+    return render_template("game.html", question = question)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
