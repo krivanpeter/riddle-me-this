fda8daa9f553b695179aec5f846caf562da2cdda
Peter Krivan <krivan.peter@outlook.com>
2018-12-17 15:24:22 +0000

    if no more riddle, app shows: 'No more riddle'

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import json
 players = {}
 
 def new_player(username):
-    players[username] = dict(points = 0, which_quest = 1)
+    players[username] = dict(attempt = 0, which_quest = 1)
 
 def write_to_file(filename):
     with open(filename, "w") as file:
@@ -18,16 +18,21 @@ def open_riddles(username, what_to_check):
     return riddles[str(which_quest)][what_to_check]
     
 def get_question(username):
-    question = open_riddles(username, "question")
+    with open("data/riddles.json") as riddle_json:
+        riddles = json.load(riddle_json)
+    if players[username]['which_quest'] > len(riddles):
+        question = "No more riddle"
+    else:
+        question = open_riddles(username, "question")
     return question
     
 def check_answer(username, got_answer):
     answer = open_riddles(username, "answer")
-    print(answer)
-    print(got_answer)
     if answer == got_answer:
         players[username]['which_quest'] += 1
+        players[username]['attempt'] += 1
         write_to_file("data/users.json")
         return True
     else:
+        players[username]['attempt'] += 1
         return False
