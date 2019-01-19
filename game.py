fa3f5006ec166fd6297b21266d4e354d193a955a
Peter Krivan <krivan.peter@outlook.com>
2019-01-19 11:21:17 +0000

    files are edited to be validated without problem

--- game.py
+++ game.py
@@ -1,30 +1,36 @@
 import difflib
 import random
 import json
-#Create dict for players
+# Create dict for players
 players = {}
 
-#If the given username already exists, this function will give 'False' back, if doesn't then create a new player in 'players' dict
+
+# If the given username already exists, this function will give 'False' back
+# if doesn't then create a new player in 'players' dict
 def new_player(username):
     if username in players:
         return False
     else:
-        players[username] = dict(attempt = 0, which_quest = 1, points = 0)
+        players[username] = dict(attempt=0, which_quest=1, points=0)
         return True
-    
-#Function to write the players into 'users.json' file 
+
+
+# Function to write the players into 'users.json' file
 def write_to_file(filename):
     with open(filename, "w") as file:
         file.write(json.dumps(players, indent=4))
 
-#Function to get to know at which riddle the player is
+
+# Function to get to know at which riddle the player is
 def open_riddles(username, what_to_check):
     which_quest = players[username]['which_quest']
     with open("data/riddles.json") as riddle_json:
         riddles = json.load(riddle_json)
     return riddles[str(which_quest)][what_to_check]
 
-#Check if there is more riddle, if not then return False, if there are return the riddle where player is    
+
+# Check if there is more riddle, if not then return False,
+# if there are return the riddle where player is
 def get_question(username):
     with open("data/riddles.json") as riddle_json:
         riddles = json.load(riddle_json)
@@ -34,8 +40,9 @@ def get_question(username):
         question = open_riddles(username, "question")
     return question
 
-#Check if answer from player and the good answer are the same, or not.
-#It increases the values of player(which_quest, attempt)
+
+# Check if answer from player and the good answer are the same, or not.
+# It increases the values of player(which_quest, attempt)
 def check_answer(username, got_answer):
     answer = open_riddles(username, "answer")
     if answer == got_answer:
@@ -47,7 +54,8 @@ def check_answer(username, got_answer):
         players[username]['attempt'] += 1
         return False
 
-#Calculate the points of the player by 'attempts' and 'which_quest'
+
+# Calculate the points of the player by 'attempts' and 'which_quest'
 def calc_points(username):
     riddles = (players[username]['which_quest']) - 1
     attempts = players[username]['attempt']
@@ -56,7 +64,9 @@ def calc_points(username):
     write_to_file("data/users.json")
     return points
 
-#Create leaderboard and gives it back, or if there was no player, then it return False    
+
+# Create leaderboard and gives it back, or if there was no player,
+# then it return False
 def create_leaderboard():
     try:
         with open("data/users.json") as users_json:
--- run.py
+++ run.py
@@ -5,66 +5,70 @@ from game import *
 
 app = Flask(__name__, static_url_path='/static')
 
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     """MAIN PAGE"""
     if request.method == "POST":
         username = request.form["username"].title()
-        #If name is not occupied then this function writes it to users.json
-        if (new_player(username)) == True:
+        # If name is not occupied then this function writes it to users.json
+        if (new_player(username)) is True:
             write_to_file("data/users.json")
-        #If name is occupied then this function renders template with 'it already exists' message
+        # If name is occupied then this function renders
+        # template with 'it already exists' message
         else:
             exist = True
-            return render_template("index.html", exist = exist)
-        return redirect(url_for("game", username = username, which_quest = players[username]['which_quest']))
+            return render_template("index.html", exist=exist)
+        return redirect(url_for("game", username=username, which_quest=players[username]['which_quest']))
     return render_template("index.html")
+
 
 @app.route('/<username>/<which_quest>', methods=["GET", "POST"])
 def game(username, which_quest):
     """GAME PAGE"""
-    #Everytime this page renders point are calculated for player and written to users.json
+    # Everytime this page renders point are calculated for player
+    # and written to users.json
     points = calc_points(username)
-    #POST happens then answer and if there is more riddles are checked
+    # POST happens then answer and if there is more riddles are checked
     if request.method == "POST":
         answer = request.form["answer"].lower()
         if check_answer(username, answer):
             question = get_question(username)
             if which_quest == 11:
-                return render_template("game.html", question = question, points = points)
+                return render_template("game.html", question=question, points=points)
             else:
-                return redirect(url_for("game", username = username, which_quest = players[username]['which_quest']))
+                return redirect(url_for("game", username=username, which_quest=players[username]['which_quest']))
         else:
-            return redirect(url_for("bad_answer", username = username, which_quest = players[username]['which_quest'], answer = answer))
+            return redirect(url_for("bad_answer", username=username, which_quest=players[username]['which_quest'], answer=answer))
     else:
         question = get_question(username)
-        return render_template("game.html", question = question, points = points)
+        return render_template("game.html", question=question, points=points)
+
 
 @app.route('/<username>/<which_quest>/<answer>', methods=["GET", "POST"])
 def bad_answer(username, which_quest, answer):
     """GAME PAGE when answer is bad"""
-    #View is created to be able to avoid a POST when page is refreshed
+    # View is created to be able to avoid a POST when page is refreshed
     points = calc_points(username)
     if request.method == "POST":
-        answer = request.form["answer"].lower()   
+        answer = request.form["answer"].lower()
         if check_answer(username, answer):
-            return redirect(url_for("game", username = username, which_quest = players[username]['which_quest']))
+            return redirect(url_for("game", username=username, which_quest=players[username]['which_quest']))
         else:
             question = get_question(username)
-            return redirect(url_for("bad_answer", username = username, which_quest = players[username]['which_quest'], answer = answer))
+            return redirect(url_for("bad_answer", username=username, which_quest=players[username]['which_quest'], answer=answer))
     else:
         question = get_question(username)
-        return render_template("game.html", question = question, answer = answer)
+        return render_template("game.html", question=question, answer=answer)
+
 
 @app.route('/leaderboard')
 def leaderboard():
     """LEADERBOARD PAGE"""
-    if (create_leaderboard()) == False:
+    if (create_leaderboard()) is False:
         return render_template("leaderboard.html")
     else:
         users = create_leaderboard()
-        return render_template("leaderboard.html", users = users)
-    
- 
+        return render_template("leaderboard.html", users=users)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
